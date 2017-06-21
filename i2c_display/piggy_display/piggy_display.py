ef587daac60e4d5cb4c7dd53b360c5da0a0103ca
yzy0004 <yisonbird@gmail.com>
2017-06-21 17:45:10 +0000

    add some display cmd

--- i2c_display/piggy_display/piggy_display.py
+++ i2c_display/piggy_display/piggy_display.py
@@ -4,8 +4,8 @@
 
 from lib_oled96 import ssd1306
 from time import sleep
-from PIL import ImageFront, ImageDraw, Image
-font = ImageFont.load_defatult()
+from PIL import ImageFont, ImageDraw, Image
+font = ImageFont.load_default()
 
 from smbus import SMBus
 i2cbus = SMBus(1)
@@ -19,6 +19,8 @@ top = padding
 bottom = oled.height - padding - 1
 
 draw.rectangle((0, 0, oled.width-1, oled.height-1), outline=1, fill=0)
+draw.text((1,1), ¨Hi Piggy¨, font=font, fill=1)
+draw.text((1,16), ¨Good Morning¨, font=font, fill=1)
 oled.display()
 
 sleep(3)
